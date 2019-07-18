ca0b7345b8adfe0ee695f5aed89b943a598fa3ea
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-18 09:15:10 -0600

    comparison

---
+++ examples/part1/02_comparison/sample.js
@@ -0,0 +1,17 @@
+const a = "1";
+const b = 1;
+const c = "1.0";
+
+//== is a convert and compare
+console.log(a == b);
+console.log(b == c);
+console.log(a == c);
+
+//=== is compare
+console.log(a === b);
+console.log(b === c);
+console.log(a === c);
+
+
+//use === instead of ==
+//use !== instead of !=
